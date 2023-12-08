38f5f03561cd90a71697af98dd8780082b84a4be
JScodeKiddie <JScodeKiddie@gmail.com>
2023-12-08 17:32:07 +0100

    add viewTransitions animations

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,3 @@
 document.querySelector(".hamburger").addEventListener("click", () => {
-  console.log("Button is clicked");
   document.querySelector(".nav-links").classList.toggle("expanded");
 });
